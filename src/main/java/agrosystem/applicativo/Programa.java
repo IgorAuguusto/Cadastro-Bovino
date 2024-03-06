ece34257e2f63ee458d9c391ae3455560f10ee82
Igor Augusto <igoraugusto36s@gmail.com>
2024-03-06 18:18:53 -0300

    Adicionada a classe DAO

--- src/main/java/agrosystem/applicativo/Programa.java
+++ src/main/java/agrosystem/applicativo/Programa.java
@@ -2,6 +2,10 @@ package agrosystem.applicativo;
 
 import java.time.LocalDate;
 
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
 import agrosystem.dominio.Bovino;
 import agrosystem.dominio.enumeracao.Raca;
 import agrosystem.dominio.enumeracao.Sexo;
@@ -16,14 +20,15 @@ public class Programa {
         // Criando instância de Bovino 2
         Bovino bovino2 = new Bovino("456DEF", "Vaca2", Situacao.SECA, Sexo.FEMININO, "789GHI", "123ABC", Raca.GIROLANDO, LocalDate.of(2017, 10, 10), LocalDate.of(2022, 6, 25), LocalDate.of(2022, 12, 20));
 
-        // Exibindo informações dos bovinos
-        System.out.println("Bovino 1:");
-        System.out.println(bovino1);
-        System.out.println();
-
-        System.out.println("Bovino 2:");
-        System.out.println(bovino2);
-        System.out.println();
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("agrosystem");
+        EntityManager em = emf.createEntityManager();
+        em.getTransaction().begin();
+        em.persist(bovino1);
+        em.persist(bovino2);
+        em.getTransaction().commit();
+        em.close();
+        emf.close();
+        
 
 	}//main()
 
---
+++ src/main/java/agrosystem/dao/DAO.java
@@ -0,0 +1,58 @@
+package agrosystem.dao;
+
+import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+import javax.persistence.criteria.CriteriaQuery;
+
+public class DAO<T> implements AutoCloseable {
+	private Class<T> classe;
+	private EntityManagerFactory entityManagerFactory;
+	private EntityManager entityManager;
+	
+	public DAO(Class<T> classe) {
+		this.classe = classe;
+		entityManagerFactory = Persistence.createEntityManagerFactory("pizzaria");
+		entityManager = entityManagerFactory.createEntityManager();
+	}//Construtor
+	
+	public void adiciona(T t) {
+		entityManager.getTransaction().begin();
+		entityManager.persist(t);
+		entityManager.getTransaction().commit();
+	}//adiciona()
+	
+	public void altera(T t) {
+		entityManager.getTransaction().begin();
+		entityManager.merge(t);
+		entityManager.getTransaction().commit();
+	}//altera()
+	
+	public void remove(T t) {
+		entityManager.getTransaction().begin();
+		entityManager.remove(entityManager.merge(t));
+		entityManager.getTransaction().commit();
+	}//remove()
+	
+	public T buscaPorId(Long id) {
+		return entityManager.find(classe, id);
+	}//buscaPorId()
+	
+	public List<T> listaTodos(){
+		CriteriaQuery<T> query = entityManager.getCriteriaBuilder().createQuery(classe);
+		
+		query.select(query.from(classe));
+		
+		List<T> list = entityManager.createQuery(query).getResultList();
+		
+		return list;
+	}//listaTodos()
+
+	@Override
+	public void close() throws Exception {
+		entityManager.close();
+		entityManagerFactory.close();
+	}//close()
+}
