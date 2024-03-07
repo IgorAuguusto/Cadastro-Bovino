13d1172752ed5a0afc5439da0dc897dd0063c349
Igor Augusto <igoraugusto36s@gmail.com>
2024-03-07 20:25:35 -0300

    Atualizada a interface de castasdro

--- src/main/java/agrosystem/gui/IgCadastro.java
+++ src/main/java/agrosystem/gui/IgCadastro.java
@@ -17,17 +17,20 @@ import javax.swing.border.LineBorder;
 import javax.swing.JLabel;
 import javax.swing.JTextField;
 import javax.swing.JComboBox;
+import javax.swing.JRadioButton;
 
 public class IgCadastro extends JDialog {
 
 	private static final long serialVersionUID = 1L;
 	private final JPanel contentPanel = new JPanel();
-	private JTextField textField;
-	private JTextField textField_1;
+	private JTextField brincoMaeField;
+	private JTextField brincoPaiField;
+	private JLabel brincoDaMaeLabel;
+	private JLabel brincoDoPaiLabel;
 
 	
 	public IgCadastro() {
-		setBounds(100, 100, 450, 562);
+		setBounds(100, 100, 390, 455);
 		getContentPane().setLayout(new BorderLayout());
 		contentPanel.setBackground(new Color(255, 255, 255));
 		getContentPane().add(contentPanel, BorderLayout.CENTER);
@@ -35,11 +38,11 @@ public class IgCadastro extends JDialog {
 		
 		JPanel panel = new JPanel();
 		panel.setBackground(new Color(255, 255, 255));
-		panel.setBounds(0, 488, 434, 35);
+		panel.setBounds(0, 380, 368, 35);
 		contentPanel.add(panel);
 		
 		JButton cadastrarButton = new JButton("Cadastrar");
-		cadastrarButton.setBounds(248, 5, 90, 28);
+		cadastrarButton.setBounds(187, 5, 90, 28);
 		cadastrarButton.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 			}
@@ -50,7 +53,7 @@ public class IgCadastro extends JDialog {
 		panel.add(cadastrarButton);
 		
 		JButton cancelarButton = new JButton("Cancelar");
-		cancelarButton.setBounds(338, 5, 90, 28);
+		cancelarButton.setBounds(278, 5, 90, 28);
 		cancelarButton.setMnemonic(KeyEvent.VK_C);
 		cancelarButton.setBackground(Color.WHITE);
 		panel.add(cancelarButton);
@@ -58,7 +61,7 @@ public class IgCadastro extends JDialog {
 		JPanel mainPanel = new JPanel();
 		mainPanel.setBorder(new TitledBorder(new LineBorder(new Color(192, 192, 192)), "Cadastro de Bovino", TitledBorder.CENTER, TitledBorder.TOP, null, new Color(0, 0, 0)));
 		mainPanel.setBackground(new Color(255, 255, 255));
-		mainPanel.setBounds(6, 0, 422, 490);
+		mainPanel.setBounds(6, 0, 362, 381);
 		contentPanel.add(mainPanel);
 		mainPanel.setLayout(null);
 		
@@ -66,30 +69,100 @@ public class IgCadastro extends JDialog {
 		brincoLabel.setBounds(16, 29, 47, 16);
 		mainPanel.add(brincoLabel);
 		
-		textField = new JTextField();
-		textField.setBounds(63, 23, 275, 28);
-		mainPanel.add(textField);
-		textField.setColumns(10);
+		JTextField brincoField = new JTextField();
+		brincoField.setBounds(63, 23, 275, 28);
+		mainPanel.add(brincoField);
+		brincoField.setColumns(10);
 		
-		textField_1 = new JTextField();
-		textField_1.setColumns(10);
-		textField_1.setBounds(63, 57, 275, 28);
-		mainPanel.add(textField_1);
+		JTextField nomeField = new JTextField();
+		nomeField.setColumns(10);
+		nomeField.setBounds(63, 57, 275, 28);
+		mainPanel.add(nomeField);
 		
-		JLabel lblNome = new JLabel("Nome:");
-		lblNome.setBounds(16, 63, 47, 16);
-		mainPanel.add(lblNome);
+		JLabel nomeLabel = new JLabel("Nome:");
+		nomeLabel.setBounds(16, 63, 47, 16);
+		mainPanel.add(nomeLabel);
 		
-		JLabel lblSituao = new JLabel("Situação:");
-		lblSituao.setBounds(16, 97, 70, 16);
-		mainPanel.add(lblSituao);
+		JLabel situaoLabel = new JLabel("Situação:");
+		situaoLabel.setBounds(16, 152, 70, 16);
+		mainPanel.add(situaoLabel);
 		
-		JComboBox comboBox = new JComboBox();
-		comboBox.setBounds(73, 92, 240, 26);
-		mainPanel.add(comboBox);
+		JComboBox situacaoComboBox = new JComboBox();
+		situacaoComboBox.setBounds(74, 147, 149, 26);
+		mainPanel.add(situacaoComboBox);
 		
-		JLabel lblSexo = new JLabel("Sexo:");
-		lblSexo.setBounds(16, 125, 52, 16);
-		mainPanel.add(lblSexo);
-	}
-}
+		brincoDaMaeLabel = new JLabel("Brinco da Mãe:");
+		brincoDaMaeLabel.setBounds(16, 91, 98, 16);
+		mainPanel.add(brincoDaMaeLabel);
+		
+		brincoMaeField = new JTextField();
+		brincoMaeField.setColumns(10);
+		brincoMaeField.setBounds(105, 85, 233, 28);
+		mainPanel.add(brincoMaeField);
+		
+		brincoDoPaiLabel = new JLabel("Brinco do Pai:");
+		brincoDoPaiLabel.setBounds(16, 124, 98, 16);
+		mainPanel.add(brincoDoPaiLabel);
+		
+		brincoPaiField = new JTextField();
+		brincoPaiField.setColumns(10);
+		brincoPaiField.setBounds(105, 119, 233, 28);
+		mainPanel.add(brincoPaiField);
+		
+		JLabel racaLabel = new JLabel("Raça:");
+		racaLabel.setBounds(16, 185, 70, 16);
+		mainPanel.add(racaLabel);
+		
+		JComboBox racaComboBox = new JComboBox();
+		racaComboBox.setBounds(74, 180, 149, 26);
+		mainPanel.add(racaComboBox);
+		
+		JLabel dataNascimentoLabel = new JLabel("Data Nascimento:");
+		dataNascimentoLabel.setBounds(16, 218, 114, 16);
+		mainPanel.add(dataNascimentoLabel);
+		
+		JTextField dataNascimentoField = new JTextField();
+		dataNascimentoField.setColumns(10);
+		dataNascimentoField.setBounds(126, 212, 212, 28);
+		mainPanel.add(dataNascimentoField);
+		
+		JLabel sexoLabel = new JLabel("Sexo:");
+		sexoLabel.setBounds(16, 246, 70, 16);
+		mainPanel.add(sexoLabel);
+		
+		JRadioButton machoNewRadioButton = new JRadioButton("Macho");
+		machoNewRadioButton.setBounds(140, 245, 83, 18);
+		mainPanel.add(machoNewRadioButton);
+		
+		JRadioButton femeaNewRadioButton = new JRadioButton("Femea");
+		femeaNewRadioButton.setBounds(235, 245, 83, 18);
+		mainPanel.add(femeaNewRadioButton);
+		
+		JLabel dataPrenhesLabel = new JLabel("Data da Prenhês:");
+		dataPrenhesLabel.setBounds(16, 274, 114, 16);
+		mainPanel.add(dataPrenhesLabel);
+		
+		JTextField dataPrenhesField = new JTextField();
+		dataPrenhesField.setColumns(10);
+		dataPrenhesField.setBounds(126, 268, 212, 28);
+		mainPanel.add(dataPrenhesField);
+		
+		JLabel dataProximoPartoLabel = new JLabel("Data Próximo Parto:");
+		dataProximoPartoLabel.setBounds(16, 308, 114, 16);
+		mainPanel.add(dataProximoPartoLabel);
+		
+		JTextField dataProximoPartoField = new JTextField();
+		dataProximoPartoField.setColumns(10);
+		dataProximoPartoField.setBounds(126, 302, 212, 28);
+		mainPanel.add(dataProximoPartoField);
+		
+		JLabel dataUltimoPartoLabel = new JLabel(" Data Último Parto:");
+		dataUltimoPartoLabel.setBounds(16, 342, 114, 16);
+		mainPanel.add(dataUltimoPartoLabel);
+		
+		JTextField dataUltimoPartoField = new JTextField();
+		dataUltimoPartoField.setColumns(10);
+		dataUltimoPartoField.setBounds(126, 336, 212, 28);
+		mainPanel.add(dataUltimoPartoField);
+	}//Construtor
+}//IgCadastro
